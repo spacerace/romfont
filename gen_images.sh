3822270fcfec3698aaf0f5eed1c46868bd6c1fe3
Nils Stec <stecdose@gmail.com>
2018-11-02 09:28:17 +0100

    work

--- gen_images.sh
+++ gen_images.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 for font in ./font-bin/*.bin
 do
 	header=`basename $font`
@@ -21,3 +20,10 @@ do
 	echo ""
 done
 
+mkdir -p ./font-images/zoomed8x8
+mkdir -p ./font-images/zoomed8x14
+mkdir -p ./font-images/zoomed8x16
+
+cp -v ./font-images/zoomed/*8x8*.png ./font-images/zoomed8x8
+cp -v ./font-images/zoomed/*8x14*.png ./font-images/zoomed8x14
+cp -v ./font-images/zoomed/*8x16*.png ./font-images/zoomed8x16
